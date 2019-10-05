5d9f1cfbcb9eb697a701ea12105119abe3da1c8d
AElMehdi <assirelmehdi@gmail.com>
2019-10-05 11:58:43 +0200

    Bootstrap the project

---
+++ Main.py
@@ -0,0 +1,11 @@
+RECIPE = "Place the cayenne, black pepper, paprika, ginger, turmeric and cinnamon into a small bowl and mix to " \
+         "combine. Place the lamb in a large bowl and toss together with half of the spice mix. Cover and leave " \
+         "overnight in the fridge"
+
+
+def process(recipe):
+    print(recipe)
+
+
+if __name__ == '__main__':
+    process(RECIPE)
