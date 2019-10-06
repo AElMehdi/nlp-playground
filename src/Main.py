040397e13c87aee778255c6b1bcf9d00d21f3e41
AElMehdi <assirelmehdi@gmail.com>
2019-10-06 14:40:05 +0200

    Add tokenizer test nltk

--- src/Main.py
+++ src/Main.py
@@ -1,10 +1,12 @@
-RECIPE = "Place the cayenne, black pepper, paprika, ginger, turmeric and cinnamon into a small bowl and mix to " \
-         "combine. Place the lamb in a large bowl and toss together with half of the spice mix. Cover and leave " \
-         "overnight in the fridge"
+import nltk
+
+RECIPE = """Place the cayenne, black pepper, paprika, ginger, turmeric and cinnamon into a small bowl and mix to
+         combine. Place the lamb in a large bowl and toss together with half of the spice mix. Cover and leave
+         overnight in the fridge"""
 
 
-def process(recipe) -> str:
-    return "Gotchya!"
+def process(recipe) -> []:
+    return nltk.word_tokenize(recipe)[:3]
 
 
 if __name__ == '__main__':
--- test/MainTest.py
+++ test/MainTest.py
@@ -4,9 +4,10 @@ from src import Main
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        result = Main.process("blomm")
-        self.assertEqual(result, "Gotchya!")
+    def test_tokenizer(self):
+        result = Main.process(Main.RECIPE)
+        self.assertEqual(result, ["Place", "the", "cayenne"])
+
 
 
 if __name__ == '__main__':
